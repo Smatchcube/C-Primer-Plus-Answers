d0096ec7be7fdfc8f2b2683d501728ab21a848a4
Smatchcube <smatchcube@tfwno.gf>
2018-12-23 22:09:45 +0100

    Added chapter 10 solutions and fixed numbering.

--- chapter_2/2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) /* not my real name and address */
-{
-	printf("My name is Gustav Mahler and I don't have an address anymore.\n");
-
-	return 0;
-}
--- chapter_3/3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) // sounds an alert and display some text
-{
-	printf("\aStartled by the sudden sound, Sally shouted,\n");
-	printf("\"By the Great Pumpkin, what was that!\"\n");
-
-	return 0;
-}
